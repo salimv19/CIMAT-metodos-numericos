75186c4f428f6da1f9a102365eb1ac0c01601a1d
Salim Vargas <lvargas7019@gmail.com>
2018-08-31 19:04:28 -0500

    Sistemas triangulares

--- Tarea2/solucion_matrices.cpp
+++ Tarea2/solucion_matrices.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 
-void imprime_vector(vector<string> vec)
+void imprime_vector(vector<double> vec)
 {
 	for (int i=0; i < vec.size(); i++)
 	{
@@ -18,9 +18,47 @@ void imprime_vector(vector<string> vec)
 	}
 }
 
-void imprime_matriz_diagonal(vector<vector<double>>& matriz)
+void imprime_matriz_diagonal_superior(vector<vector<double>>& matriz, int& N, int& M)
 {
-	//bla bla bla
+	cout << endl;
+	for (int i=0; i < N; i++)
+	{
+		for (int j=0; j < M; j++)
+		{
+			if (j < i)
+			{
+				cout << "\t    0\t";
+			}
+			else
+			{
+				cout << "\t" << matriz[i][j-i] << "   ";
+			}
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+vector<double> lee_vector(string nombreArchivo, int& N)
+{
+	vector<double> vector;
+	double auxNum;
+	ifstream entrada (nombreArchivo.c_str());
+
+	if (entrada.is_open())
+	{
+  		entrada >> N;
+	  	for (int i=0; i < N; i++)
+	  	{
+	  		entrada >> auxNum;
+	  		vector.push_back(auxNum);
+	  	}
+	  	entrada.close();
+	}
+	else
+  		cout << "ERROR al leer archivo\n";
+	
+	return vector;
 }
 
 vector<double> lee_matriz_diagonal(string nombreArchivo, int& N, int& M)
@@ -33,10 +71,68 @@ vector<double> lee_matriz_diagonal(string nombreArchivo, int& N, int& M)
 	{
   		entrada >> N;
   		entrada >> M;
-	  	while (!entrada.eof())
+	  	for (int i=0; i < N; i++)
 	  	{
 	  		entrada >> auxNum;
 	  		matriz.push_back(auxNum);
+	  	}
+	  	entrada.close();
+	}
+	else
+  		cout << "ERROR al leer archivo\n";
+	
+	return matriz;
+}
+
+vector<vector<double>> lee_matriz_triangular_superior(string nombreArchivo, int& N, int& M)
+{
+	vector<vector<double>> matriz;
+	vector<double> renglon;
+	double auxNum;
+	ifstream entrada (nombreArchivo.c_str());
+
+	if (entrada.is_open())
+	{
+  		entrada >> N;
+  		entrada >> M;
+	  	for (int i=0; i < N; i++)
+	  	{
+	  		for (int j=i; j < M; j++)
+	  		{
+	  			entrada >> auxNum;
+	  			renglon.push_back(auxNum);
+	  		}
+	  		matriz.push_back(renglon);
+	  		renglon.clear();
+	  	}
+	  	entrada.close();
+	}
+	else
+  		cout << "ERROR al leer archivo\n";
+	
+	return matriz;
+}
+
+vector<vector<double>> lee_matriz_triangular_inferior(string nombreArchivo, int& N, int& M)
+{
+	vector<vector<double>> matriz;
+	vector<double> renglon;
+	double auxNum;
+	ifstream entrada (nombreArchivo.c_str());
+
+	if (entrada.is_open())
+	{
+  		entrada >> N;
+  		entrada >> M;
+	  	for (int i=0; i < N; i++)
+	  	{
+	  		for (int j=0; j <= i; j++)
+	  		{
+	  			entrada >> auxNum;
+	  			renglon.push_back(auxNum);
+	  		}
+	  		matriz.push_back(renglon);
+	  		renglon.clear();
 	  	}
 	  	entrada.close();
 	}
@@ -65,6 +161,56 @@ vector<double> resuelve_sistema_diagonal(vector<double>& matriz, vector<double>&
 	return x;
 }
 
+vector<double> resuelve_sistema_triangular_superior(vector<vector<double>>& matriz, vector<double>& b, int& N, int& M)
+{
+	vector<double> x(N);
+	double suma;
+	
+	for (int i=0; i < N; i++)
+	{
+		if (matriz[N-1-i][0] != 0)
+		{
+			suma = 0;
+			for (int j=0; j < i; j++)
+			{
+				suma = suma + matriz[N-1-i][i-j]*x[N-1-j];
+			}
+			x[i] = (b[i] - suma)/matriz[N-1-i][0];
+		}
+		else
+		{
+			cout << "\tERROR. Los elementos de la diagonal contienen ceros\n";
+		}
+	}
+
+	return x;
+}
+
+vector<double> resuelve_sistema_triangular_inferior(vector<vector<double>>& matriz, vector<double>& b, int& N, int& M)
+{
+	vector<double> x(N);
+	double suma;
+	
+	for (int i=0; i < N; i++)
+	{
+		if (matriz[i][i] != 0)
+		{
+			suma = 0;
+			for (int j=0; j < i; j++)
+			{
+				suma = suma + matriz[i][j]*x[j];
+			}
+			x[i] = (b[i] - suma)/matriz[i][i];
+		}
+		else
+		{
+			cout << "\tERROR. Los elementos de la diagonal contienen ceros\n";
+		}
+	}
+
+	return x;
+}
+
 vector<string> lee_nuevo_email(string nombreArchivo, vector<string>& diccionario)
 {
 	vector<string> palabras, nuevasPalabras;
@@ -76,10 +222,8 @@ vector<string> lee_nuevo_email(string nombreArchivo, vector<string>& diccionario
 	  	while (!entrada.eof())
 		{
 			entrada >> palabra;
-			palabra = quita_caracteres(palabra);
-	  		transform(palabra.begin(), palabra.end(), palabra.begin(), ::tolower);
 			
-			if (palabra.length() > 3 & find(palabras.begin(), palabras.end(), palabra) == palabras.end())
+			if (palabra.length() > 3)
 	  		{
 	  			palabras.push_back(palabra);
 	  		}
@@ -88,9 +232,6 @@ vector<string> lee_nuevo_email(string nombreArchivo, vector<string>& diccionario
 	}
 	else
   		cout << "ERROR al leer archivo\n";
-	
-	sort(palabras.begin(), palabras.end());
-	interseca_diccionarios(palabras, diccionario, nuevasPalabras);
 
 	return nuevasPalabras;
 }
@@ -121,7 +262,24 @@ void imprime_resultados(vector<string>& palabras, vector<double>& probabilidades
 
 int main()
 {
-	
+	vector<double> matrizD, b, x;
+	vector<vector<double>> matrizTS, matrizTI, matrizC;
+	int N, M;
+
+	/*matrizD = lee_matriz_diagonal("TAREA2_MATRICES/DIAG_MAT500.txt", N, M);
+	b = lee_vector("TAREA2_MATRICES/DIAG_VEC500.txt", M);
+	x = resuelve_sistema_diagonal(matrizD, b, N);*/
+
+	/*matrizTS = lee_matriz_triangular_superior("TAREA2_MATRICES/TSUP_MAT500.txt", N, M);
+	b = lee_vector("TAREA2_MATRICES/TSUP_VEC500.txt", M);
+	x = resuelve_sistema_triangular_superior(matrizTS, b, N, M);*/
+
+	matrizTI = lee_matriz_triangular_inferior("TAREA2_MATRICES/TINF_MAT4.txt", N, M); imprime_matriz_diagonal_superior(matrizTI, N, M);
+	b = lee_vector("TAREA2_MATRICES/TINF_VEC4.txt", M);
+	x = resuelve_sistema_triangular_inferior(matrizTI, b, N, M);
+
+	imprime_matriz_diagonal_superior(matrizTI, N, M);
+	imprime_vector(x);
 
 	return 0;
 }
